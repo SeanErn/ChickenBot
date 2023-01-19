4b76118946da12dc4b44b323cc5a52db4352e14e
SeanErn <seanernstes@gmail.com>
2023-01-18 22:07:35 -0500

    update command

--- commands/chickengame.js
+++ commands/chickengame.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, Events } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, Events, Message } = require("discord.js");
 const { MongoClient } = require("mongodb");
 const serverUri = process.env["SERVERURI"]
 
@@ -41,8 +41,9 @@ module.exports = {
                             var postGameP4 = `4. ${playerfour} had ${dbPlayerfour.chickensRemaining} chickens left \n`;
                             players = [interaction.options.getUser("playerone"), interaction.options.getUser("playertwo"), interaction.options.getUser("playerthree"), interaction.options.getUser("playerfour")];
                         }
-            
-                        var currentPlayer = interaction.options.getUser("playerone");
+
+                        var currentPlayerPos = 0;
+                        var currentPlayer = players[currentPlayerPos];
                         var currentDare = 1;
 
 
@@ -67,6 +68,8 @@ module.exports = {
                     await coll.updateOne({userId:player.id},{$set: {gameInProgress:true,gameDateStarted:Date.now(), chickensRemaining:interaction.options.getInteger("numberofchickens")}});                 
                 }
             });
+            
+
             //When the game is over, update the database
             const gameEnd = async () => {
                 const db = dbClient.db("chickenBotUserData");
@@ -76,6 +79,21 @@ module.exports = {
                     dbPlayer = await coll.findOne({userId:player.id});
                 await coll.updateOne({userId:player.id},{$set: {gameInProgress:false}});
                 });
+            }
+            const nextPlayer = async () => {
+                if (players[players.indexOf(currentPlayer)+1] == undefined){
+                    currentPlayerPos = 0;
+                    currentPlayer = players[currentPlayerPos];
+                } else {
+                    currentPlayerPos++;
+                    currentPlayer = players[currentPlayerPos];
+                }
+            }
+            const chickenOut = async () => {
+                const db = dbClient.db("chickenBotUserData");
+                const coll = db.collection("chickenBotUserData");
+                dbPlayer = await coll.findOne({userId:currentPlayer.id});
+                await coll.updateOne({userId:currentPlayer.id},{$set: {chickensRemaining:dbPlayer.chickensRemaining-1}});
             }
 
     
@@ -106,31 +124,23 @@ module.exports = {
                     {name: "Rules", value: `1. The game will start with a random player. \n 2. The player will be given a dare. \n 3. The player will then have 15 seconds to decide if they will chicken out or not. \n 4. If they chicken out, they will be given a new dare and will lose one of their ${interaction.options.getInteger("numberofchickens")} chickens \n 5. If they do not chicken out, they must complete the dare \n 6. The game will continue until you finish ${interaction.options.getInteger("numberofdares")} collectively \n 7. Each player starts with ${interaction.options.getInteger("numberofchickens")} chickens. \n`}
                 );
 
+                const genDare = async () => {
             const dareDb = dbDareClient.db("chickenBotChickens");
             const dareColl = dareDb.collection("chickenBotChickens"); 
             var userInDB = await coll.findOne({userId:currentPlayer.id});
-            var dare = await dareColl.findOne({INDEX:`${Math.random() * (Math.floor(26) - Math.ceil(0)) + Math.ceil(0)}`})
-                    console.log(dare);
+            var dare = await dareColl.findOne({INDEX:Math.floor(Math.random() * (26 - 0) + 0)});
+                    //console.log(dare);
             const duringGame = new EmbedBuilder()
                 .setTitle("Chicken Game")
-                .setAuthor({ name: `${currentPlayer.username}`})
+                .setAuthor({ name: `${currentPlayer.username}`, iconURL: currentPlayer.displayAvatarURL()})
                 .setDescription(`It is ${currentPlayer.username}'s turn!`)
                 .addFields(
                     {name: "Dare Number", value: `${currentDare}/${interaction.options.getInteger("numberofdares")}`},
                     {name: "Chicken's remaining", value: `${userInDB.chickensRemaining} chickens`},
-                    {name: "Dare", value: `${dare}`},
+                    {name: "Dare", value: `${dare.Q} (#${dare.INDEX})`},
                 );
-
-                var dbPlayerone = await coll.findOne({userId:interaction.options.getUser("playerone").id});
-                var dbPlayertwo = await coll.findOne({userId:interaction.options.getUser("playertwo").id});
-            const postGame = new EmbedBuilder()
-                .setTitle("Chicken Game")
-                .setAuthor({ name: interaction.user.username, iconURL: interaction.user.displayAvatarURL()})
-                .setDescription("The game has ended! Thanks for playing!")
-                .addFields(
-                    {name: "Results", value: `1. ${interaction.options.getUser("playerone")} had ${dbPlayerone.chickensRemaining } chickens left \n 2. ${interaction.options.getUser("playertwo")} had ${dbPlayertwo.chickensRemaining} chickens left \n ${postGameP3} ${postGameP4}`}
-                );
-                
+                return duringGame;
+            }                
 
             //Build the buttons
             const pregame = new ActionRowBuilder()
@@ -173,13 +183,30 @@ module.exports = {
             const collectorPregame = interaction.channel.createMessageComponentCollector({ filter, time: 600*1000 });
             
             collectorPregame.on('collect', async i => {
+
+                    if (currentDare == interaction.options.getInteger("numberofdares")+1){
+                        var dbPlayerone = await coll.findOne({userId:interaction.options.getUser("playerone").id});
+                        var dbPlayertwo = await coll.findOne({userId:interaction.options.getUser("playertwo").id});
+
+                        const postGame = new EmbedBuilder()
+                        .setTitle("Chicken Game")
+                        .setAuthor({ name: interaction.user.username, iconURL: interaction.user.displayAvatarURL()})
+                        .setDescription("The game has ended! Thanks for playing!")
+                        .addFields(
+                            {name: "Results", value: `1. ${interaction.options.getUser("playerone")} had ${dbPlayerone.chickensRemaining } chickens left \n 2. ${interaction.options.getUser("playertwo")} had ${dbPlayertwo.chickensRemaining} chickens left \n ${postGameP3} ${postGameP4}`}
+                        );
+
+                        await i.update({ embeds: [postGame], components: [] });
+                     } 
                     //PREGAME
-                    if (i.customId === 'startgame_PREGAME') {
+                    else if (i.customId === 'startgame_PREGAME') {
                         await i.update({ embeds: [gameRules], components: [pregameRules] });
                     }
                     //PREGAME RULES
                     else if (i.customId === 'next_PREGAMERULES') {
-                        await i.update({ embeds: [duringGame], components: [ingame] });
+                        await i.update({ embeds: [await genDare()], components: [ingame] });
+                        await nextPlayer();
+                        currentDare++;
                     }
                     else if (i.customId === 'stopgame_PREGAMERULES') {
                         await i.update({ embeds: [postGame], components: [] });
@@ -187,10 +214,15 @@ module.exports = {
                     }
                     //INGAME
                     else if (i.customId === 'next_INGAME') {
-                        await i.update({ embeds: [duringGame], components: [ingame] });
+                        await i.update({ embeds: [await genDare()], components: [ingame] });
+                        await nextPlayer();
+                        currentDare++;
                     }
                     else if (i.customId === 'chickenout') {
-                        await i.update({ embeds: [gameRules], components: [ingame] });
+                        await i.update({ embeds: [await genDare()], components: [ingame] });
+                        await nextPlayer();
+                        await chickenOut();
+                        currentDare++;
                     }
                     else if (i.customId === 'stopgame_INGAME') {
                         await i.update({ embeds: [postGame], components: [] });
